95ccb61ec67e931248d5ffd06e0ede11c6cb2190
Amy <152795851+amyf211@users.noreply.github.com>
2024-07-01 12:01:31 +0100

    Create MusicManager.cs

---
+++ MusicManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private AudioSource _audiosource;
+    public AudioClip[] songs;
+    public float volume;
+    [SerializeField] private float _songsPlayed;
+    [SerializeField] private bool[] _beenPlayed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audiosource = GetComponent<AudioSource>();
+
+	_beenPlayed = new bool[songs.Length];
+
+	if(!_audiosource.isPlaying)
+		ChangeSong(Random.Range(0,songs.Length));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _audiosource.volume = volume;
+
+	if(!_audiosource.isPlaying)
+		ChangeSong(Random.Range(0,songs.Length));
+	
+	if(_songsPlayed == songs.Length)
+	{
+	    _songsPlayed = 0;
+	    for (int i = 0; i < songs.Length; i++)
+	    {
+		if (i == songs.Length)
+		    break;
+		else
+		    _beenPlayed[i] = false;
+	    }
+	}
+    }
+
+    public void ChangeSong(int songPicked)
+    {	
+	if(!_beenPlayed[songPicked])
+	{
+	    _songsPlayed++;
+	    _beenPlayed[songPicked] = true;
+	    _audiosource.clip = songs[songPicked];
+            _audiosource.Play();
+	}
+	else
+	    _audiosource.Stop();
+    }
+}
